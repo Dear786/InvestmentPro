6bbe038379e5f72489ec7e6fab8bb8e8102d1d36
Dear786 <asifali116bw@gmail.com>
2026-06-02 19:16:45 +0500

    Add Firebase configuration for Firestore

---
+++ www/js/firebase-config.js
@@ -0,0 +1,8 @@
+const db = firebase.initializeApp({
+    apiKey: "AIzaSyAzXuj8AvwK500YqMXcXP-LhMVKmAr6KEs",
+    authDomain: "investment-pro-1aba9.firebaseapp.com",
+    projectId: "investment-pro-1aba9",
+    storageBucket: "investment-pro-1aba9.firebasestorage.app",
+    messagingSenderId: "288047785670",
+    appId: "1:288047785670:web:104ac80cb6b5ca98f559c7"
+}).firestore();
